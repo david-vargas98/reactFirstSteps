974ed093e7684d9c84ed93200a4caa079261e5d1
david-vargas98 <edgar981125@live.com.mx>
2025-08-30 11:25:12 -0600

    refactor/feat: relocating properties out of the component and adding a styles object.

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,15 +1,24 @@
+import type { CSSProperties } from "react";
+
+const firstName = "Edgar";
+const lastName = "Vargas";
+
+const myFavoriteGames = ['Halo', 'Ninja Gaiden', 'Doom'];
+const isActive = true;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canada'
+}
+
+const myStyles: CSSProperties = {
+    backgroundColor: 'red',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 50
+}
+
 export function MyAwesomeApp() {
-    const firstName = "Edgar";
-    const lastName = "Vargas";
-
-    const myFavoriteGames = ['Halo', 'Ninja Gaiden', 'Doom'];
-    const isActive = true;
-
-    const address = {
-        zipCode: 'ABC-123',
-        country: 'Canada'
-    }
-
     return (
         <>
             <h1>{firstName}</h1>
@@ -19,11 +28,7 @@ export function MyAwesomeApp() {
 
             <h1>{isActive ? 'Active' : 'Not active'}</h1>
 
-            <p style={{
-                backgroundColor: 'red',
-                borderRadius: 20,
-                padding: 10
-            }}> {JSON.stringify(address)} </p>
+            <p style={myStyles}> {JSON.stringify(address)} </p>
         </>
     );
 }
