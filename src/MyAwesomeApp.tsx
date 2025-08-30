67e4fd62a30b5f892b79530d2d4f415f63898969
david-vargas98 <edgar981125@live.com.mx>
2025-08-30 11:05:38 -0600

    feat: printing variable sinto the component.

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
 export function MyAwesomeApp() {
-    return(
+    const firstName = "Edgar";
+    const lastName = "Vargas";
+
+    const myFavoriteGames = ['Halo', 'Ninja Gaiden', 'Doom'];
+    const isActive = true;
+
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'Canada'
+    }
+
+    return (
         <>
-            <h1>Edgar</h1>
-            <h3>Vargas</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{ myFavoriteGames.join(', ') }</p>
+
+            <h1>{ isActive ? 'Active' : 'Not active' }</h1>
+
+            <p>
+                { JSON.stringify(address) }
+            </p>
         </>
-    )
+    );
 }
