1a60fa2158d632b05c16b9928255e77f4e08e882
david-vargas98 <edgar981125@live.com.mx>
2025-08-30 10:08:19 -0600

    feat: my first component.

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <p>This is a paragraph</p>
+
+            <button>Click me</button>
+
+            <div>
+            <h2>Hello from inside a div!</h2>
+            </div>
+        </>
+    )
+}
