ed177a88d178667c6175cecb29b46eb198845bdb
david-vargas98 <edgar981125@live.com.mx>
2025-08-30 18:29:25 -0600

    feat: Dynamic prop valuess.
    Making dynamic the prop values assignment by using an array.

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,13 +1,29 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface itemInCart {
+    productName: string;
+    quantity: number;
+};
+
+const itemsInCart: itemInCart[] = [
+    {productName: "Nintendo switch 2", quantity: 1},
+    {productName: "Pro controller", quantity:3},
+    {productName: "Super smash", quantity:5}
+]
+
+
 export function FirstStepsApp() {
     return (
         <>
             <h1>Shopping cart</h1>
-
-            <ItemCounter name="Nintendo switch 2" quantity={1} />
+            {
+                itemsInCart.map(({productName, quantity}) => (
+                    <ItemCounter name={productName} quantity={quantity} />
+                ))
+            }
+            {/* <ItemCounter name="Nintendo switch 2" quantity={1} />
             <ItemCounter name="Pro controller" quantity={1} />
-            <ItemCounter name="Super smash" quantity={1} />
+            <ItemCounter name="Super smash" quantity={1} /> */}
         </>
     )
 }
