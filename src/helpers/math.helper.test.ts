55f1242ea12549ad6d794ff05035e33bdb69fb72
david-vargas98 <edgar981125@live.com.mx>
2025-08-31 12:00:04 -0600

    feat: making subtract and multiply tasks about testing.

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,15 +1,85 @@
 // Rule of thump, a test file must contain at least one test, otherwise it will fail
-import { expect, test } from 'vitest';
-import { add } from './math.helper';
+import { describe, expect, test } from 'vitest';
+import { add, multiply, subtract } from './math.helper';
 
-test('should add two positive numbers', () => {
-    //! 1. Arrange
-    const a = 1;
-    const b = 2;
+// we use describe to group tests
 
-    //! 2. Act
-    const result = add(a, b);
+//add testing
+describe('add', () => {
+    test('should add two positive numbers', () => {
+        //! 1. Arrange
+        const a = 1;
+        const b = 2;
+    
+        //! 2. Act
+        const result = add(a, b);
+    
+        //! 3. Assertion
+        expect(result).toBe(a + b);
+    });
+    test('should add two negative numbers', () => {
+        //! 1. Arrange
+        const a = -4;
+        const b = -5;
+    
+        //! 2. Act
+        const result = add(a, b);
+    
+        //! 3. Assertion
+        expect(result).toBe(a + b);
+    });
+});
 
-    //! 3. Assertion
-    expect(result).toBe(a + b);
+//  Challenges:
+
+//Subtract testing
+describe('subtract', () => {
+    test('should subtract two positive numbers', () => {
+        //! 1. Arrange
+        const a = 4;
+        const b = 5;
+
+        //! 2.Act
+        const result = subtract(a, b);
+
+        //! 3.Assertion
+        expect(result).toBe(a - b);
+    });
+    test('should subtract two negative numbers', () => {
+        //! 1. arrange
+        const a = -7;
+        const b = -9;
+
+        //! 2. Act
+        const result = subtract(a, b);
+
+        //! 3. Assertion
+        expect(result).toBe(a - b);
+    });
+});
+
+//multiply testing
+describe('multiply', () => {
+    test('multiply one positive and one negative number', () => {
+        //! 1. Arrange
+        const a = 10;
+        const b = -54;
+
+        //! 2. Act
+        const result = multiply(a, b);
+
+        //! 3. Assertion
+        expect(result).toBe(a * b);
+    });
+    test('multiply two negative numbers', () => {
+        //! 1. Arrange
+        const a = 43;
+        const b = 33;
+
+        //! 2. Act
+        const result = multiply(a, b);
+
+        //! 3. Assertion
+        expect(result).toBe(a * b);
+    });
 });
