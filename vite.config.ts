01ec1ec8f335556cef7a8b086f68c5c34570b803
david-vargas98 <edgar981125@live.com.mx>
2025-08-31 18:41:33 -0600

    build: adding vitest environment and lobals values.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'; // we need to import it from vitest to be able to test components using testing library
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom', // we need to restart the command 'npm run test' and install this dependency
+    globals: true
+  }
 })
