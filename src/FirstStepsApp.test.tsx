c6533960234ef2b186fcd3760ad73d8c2cb3859c
david-vargas98 <edgar981125@live.com.mx>
2025-09-02 01:56:11 -0600

    feat: implementing testing for the render of child components by using a mock.

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,13 +1,30 @@
-import { describe, expect, test } from 'vitest';
-import { render ,screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 import { FirstStepsApp } from './FirstStepsApp';
+
+// mock creation for ItemCounter: this allows us to keep the ItemCounter dependency but without rendering it, it's a simulation of it:
+vi.mock("./shopping-cart/ItemCounter", () => ({
+    ItemCounter: () => <div data-testid="ItemCounter" />,
+}));
+
 
 // Challenge: make the snapshot from the component
 describe('FirstStepsApp', () => {
     test('should match snapshot', () => { //instructor says won't show this test anymore for the rest of the course
+        //! 1. Arrange and 2. Act (implicitly)
         render(<FirstStepsApp />);
 
+        //! 3. Assert
         expect(screen.getAllByRole('root')).toMatchSnapshot();
-    })
+    });
+
+    test('should render the correct number of ItemCounter components', () => {
+        //! 1. Arrange
+        render(<FirstStepsApp />);
+
+        const itemCounters = screen.getAllByTestId('ItemCounter'); // getting by 'data-testid'
+
+        expect(itemCounters.length).toBe(3);
+    });
 });
